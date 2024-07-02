14abd1dc4416a8fcff9ce186c2c067270ae4bb01
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-02 22:17:13 +0530

    again meteors

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex justify-center items-center'>
-        
+        <Meteors/>
        <Background/>
     </div>
   )
