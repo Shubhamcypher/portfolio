64a82fae0ca8bc4abca2c0623844dcbeb5b1ea36
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-05 00:48:52 +0530

    adding background to experience

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,59 +1,66 @@
-// vertical timeline component
-
-"use client";
-
-import React from "react";
+'use client';
+import React from 'react';
 
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import experienceData from "@/data/experience_details.json";
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import experienceData from '@/data/experience_details.json';
+import { useInView } from 'react-intersection-observer';
+import { useSectionInView } from '@/hooks/useSectionInView';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
-import { useInView } from "react-intersection-observer";
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import HandshakeOutlinedIcon from '@mui/icons-material/HandshakeOutlined';
+import { Boxes } from '@/components/ui/background-boxes';
+import { WavyBackground } from '@/components/ui/wavy-background';
+
 
 export default function Experience() {
-
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-  });
-
+  const { ref } = useSectionInView('Experience');
   return (
-    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40 mt-20">
-      <VerticalTimeline lineColor="#00f">
-        {experienceData.experience.map((item, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement
-              visible={inView}
-              contentStyle={{
-                background: "#f3f4f0" ,
-                boxShadow: "none",
-                border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
-              }}
-              contentArrowStyle={{
-                borderRight: "0.4rem solid rgba(255, 255, 255, 0.5)",
-              }}
-              date={item.date}
-              icon={<AccountBalanceIcon/>}
-              iconStyle={{
-                background : "rgba(255, 255, 255, 0.15)",
-                fontSize: "1.5rem",
-              }}
-            >
-              <h2 className="font-semibold capitalize text-red-500">{item.title}</h2>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                {item.description}
-              </p>
-            </VerticalTimelineElement>
-          </React.Fragment>
-        ))}
+    <section
+      ref={ref}
+      id="experience"
+      className="mt-28  text-center sm:mb-10 flex justify-center items-center w-screen max-h-full z-0"
+    >
+      <Boxes/>
+      <WavyBackground waveOpacity={1} speed='slow' blur={0.1}/>
+      <VerticalTimeline lineColor="#00ff00">
+        {experienceData.experience.map((item, index) => {
+          const { ref, inView } = useInView({
+            
+          });
+          return (
+            <div key={index} ref={ref} className="vertical-timeline-element">
+              <VerticalTimelineElement
+                contentStyle={{
+                  background: 'rgba(255, 255, 255, 0.025)',
+                  boxShadow: 'none',
+                  border: '2px solid rgba(0, 0, 0, 0.55)',
+                  textAlign: 'left',
+                  padding: '1.3rem 2rem',
+                }}
+                contentArrowStyle={{
+                  borderRight: '0.6rem solid green',
+                }}
+                visible={inView}
+                date={item.date}
+                icon={<HandshakeOutlinedIcon/>}
+                iconStyle={{
+                  background: item.background,
+                }}
+              >
+                <h3 className="font-extrabold capitalize text-red-600 text-2xl">{item.title}</h3>
+                <p className="font-normal text-blue-500">{item.location}</p>
+                <p className="!font-normal text-gray-400">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            </div>
+          );
+        })}
       </VerticalTimeline>
     </section>
   );
 }
-
-
---
+++ hooks/useSectionInView.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useSectionInView = (id) => {
+  const ref = useRef(null);
+  const [inView, setInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setInView(entry.isIntersecting);
+      },
+      {
+        threshold: 0.1, // Adjust this value as needed
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return { ref, inView };
+};
