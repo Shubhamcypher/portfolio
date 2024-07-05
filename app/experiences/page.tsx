67c816df583f07513f0a815f8965c749f5addb42
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-05 16:24:37 +0530

    adding education

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -7,12 +7,15 @@ import { useSectionInView } from '@/hooks/useSectionInView';
 import { Boxes } from '@/components/ui/background-boxes';
 import { WavyBackground } from '@/components/ui/wavy-background';
 import TimelineElement from '@/components/TimelineElement';
+import { HoverEffect } from '@/components/ui/card-hover-effect';
+import educDetails from '@/data/education_details.json'
 
 export default function Experience() {
   const { ref } = useSectionInView('Experience');
 
   return (
-    <section
+    <div>
+      <section
       ref={ref}
       id="experience"
       className="mt-28 text-center sm:mb-10 flex justify-center  items-center w-full max-h-full z-0"
@@ -24,6 +27,11 @@ export default function Experience() {
           <TimelineElement key={index} item={item} />
         ))}
       </VerticalTimeline>
-    </section>
+      </section>
+      <h1 className='text-2xl md:text-4xl lg:text-7xl text-purple-600  text-center font-extrabold'>Education</h1>
+      <div>
+        <HoverEffect items={educDetails.education}/>
+      </div>
+    </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,27 +25,27 @@ function Navbar({ className }: { className?: string }) {
             <MenuItem active={active} setActive={setActive} item="Skills">
                 <div className="flex flex-col text-sm space-y-4 ">
                         <div className="flex flex-shrink items-center justify-between">
-                            <HoveredLink href="/projects">React</HoveredLink>
+                            <HoveredLink href="/skills">React</HoveredLink>
                             <img src="https://pluspng.com/img-png/react-logo-png-react-logo-png-transparent-amp-svg-vector-pluspng-2400x2400.png" alt="react" className="rounded-full w-6 h-6 "/>
                         </div>
                         <div className="flex flex-shrink items-center justify-between">
-                            <HoveredLink href="/projects">Node JS</HoveredLink>
+                            <HoveredLink href="/skills">Node JS</HoveredLink>
                             <img src="https://cdn4.iconfinder.com/data/icons/logos-3/454/nodejs-new-pantone-white-512.png" alt="node" className="rounded-full w-6 h-6"/>
                         </div>
                         <div className="flex flex-shrink items-center justify-between">
-                            <HoveredLink href="/projects">Mongo DB</HoveredLink>
+                            <HoveredLink href="/skills">Mongo DB</HoveredLink>
                             <img src="https://i.pinimg.com/originals/90/3b/bb/903bbb31b5ae50326ae7d2eac38c9a33.jpg" alt="mongodb"  className="rounded-full w-6 h-6"/>
                         </div>
                         <div className="flex flex-shrink items-center justify-between">
-                            <HoveredLink href="/projects">MySQL</HoveredLink>
+                            <HoveredLink href="/skills">MySQL</HoveredLink>
                             <img src="https://p7.hiclipart.com/preview/572/182/378/mysql-relational-database-management-system-logo-microsoft-sql-server-mysql-logo.jpg" alt="mysql"  className="rounded-full w-6 h-6"/>
                         </div>
                         <div className="flex flex-shrink items-center justify-between">
-                            <HoveredLink href="/projects">Next JS</HoveredLink>
+                            <HoveredLink href="/skills">Next JS</HoveredLink>
                             <img src="https://w7.pngwing.com/pngs/87/586/png-transparent-next-js-hd-logo.png" alt="next"  className="rounded-full w-6 h-6"/>
                         </div>
                         <div className="flex flex-shrink items-center justify-between gap-5">
-                            <HoveredLink href="/projects">Virtual Box</HoveredLink>
+                            <HoveredLink href="/skills">Virtual Box</HoveredLink>
                             <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Virtualbox_logo.png" alt="virtual box"  className="rounded-full w-6 h-6"/>
                         </div>
                     </div>
