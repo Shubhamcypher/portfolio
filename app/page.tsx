71a7853c1c4162863163414b94a6496c91219dc1
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-05 23:20:17 +0530

    gfhfgh

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 'use client'
-
-
-import Background from "@/components/Background";
 import { Intro } from "@/components/Intro";
-import Image from "next/image";
 
 export default function Home() {
   return (
