c18a0b3cbec6e4c5d697493c06264525a57a4162
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-05 22:46:43 +0530

    adding contact backend

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,20 +1,66 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
+import contact from '../';
 
 const ContactMe = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [status, setStatus] = useState('');
+
+  const handleChange = (e:any) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      console.log("hey");
+      
+      const response = await fetch('/api/contact.js', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setStatus('Message sent successfully!');
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+      } else {
+        setStatus('Error sending message. Please try again.');
+      }
+    } catch (error) {
+      setStatus('Something went wrong. Please try again');
+    }
+  };
+
   return (
     <div className="mt-8 relative flex items-center justify-center bg-transparent z-50">
       <div className="bg-gray-900 bg-opacity-20 p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold text-white mb-6 text-center">Contact Me</h2>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name" className="block text-sm font-medium text-white">Name</label>
             <input
               type="text"
               id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
               className="mt-1 block w-full px-4 py-2 bg-gray-700 text-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
           </div>
@@ -23,6 +69,9 @@ const ContactMe = () => {
             <input
               type="email"
               id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
               className="mt-1 block w-full px-4 py-2 bg-gray-700 text-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
           </div>
@@ -30,30 +79,34 @@ const ContactMe = () => {
             <label htmlFor="message" className="block text-sm font-medium text-white">Message</label>
             <textarea
               id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
               className="mt-1 block w-full px-4 py-2 bg-gray-500 text-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
               rows={4}
             ></textarea>
           </div>
           <div className="flex justify-between items-center">
             <button
-              type="submit"
+              onClick={handleSubmit}
               className="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             >
               Send Message
             </button>
             <div className="flex space-x-4">
-              <Link href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <Link href="https://www.linkedin.com/in/shubham-kumar-58680a215" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} className="text-white text-3xl hover:text-blue-500" />
               </Link>
-              <Link href="https://github.com" target="_blank" rel="noopener noreferrer">
+              <Link href="https://github.com/Shubhamcypher" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faGithub} className="text-white text-3xl hover:text-green-500" />
               </Link>
-              <Link href="https://github.com" target="_blank" rel="noopener noreferrer">
+              <Link href="mailto:shubhamdevu023@gmail.com" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faEnvelope} className="text-white text-3xl hover:text-red-500" />
               </Link>
             </div>
           </div>
         </form>
+        {status && <p className="mt-4 text-center text-red-600 font-semibold text-xl">{status}</p>}
       </div>
     </div>
   );
---
+++ pages/api/contact.js
@@ -0,0 +1,37 @@
+'use client'
+import nodemailer from 'nodemailer';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    console.log("I am here");
+    
+    const { name, email, message } = req.body;
+
+    // Create a transporter
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'shubhamdevu023@gmail.com',
+        pass: 'Shubham@123',
+      },
+    });
+
+    // Email options
+    const mailOptions = {
+      from: email,
+      to: 'shubhamdevu023@gmail.com',
+      subject: `New message from ${name}`,
+      text: message,
+    };
+
+    try {
+      // Send email
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ success: true });
+    } catch (error) {
+      res.status(500).json({ error: 'Error sending email' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+};
