079924b07760f95aee9423b22353af85d09cf9fb
shubhamcypher <shubhamdevo23@gmail.com>
2024-07-06 15:26:06 +0530

    adding lamp with size factor

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -19,7 +19,25 @@ const me = [
 
 export function Intro() {
   return (
-    <div>
+    <div >
+      <div className="lg:hidden">
+      <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Hi! <br /> I am Shubham
+      </motion.h1>
+    </LampContainer>
+      </div>
+
+      <div className=" relative">
       <Boxes/>
       <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <motion.h1
@@ -67,6 +85,7 @@ export function Intro() {
       
     </div>
       
+    </div>
     </div>
   );
 }
